67d038d9f56200382e3c674b9f166b16fffb88e3
EdShel <eduardchelemetjev@gmail.com>
2020-09-27 17:53:29 +0300

    Updated template file

--- CsFromFolder/ExampleTemplates/Template.cs
+++ CsFromFolder/ExampleTemplates/Template.cs
@@ -1,22 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Example
 {
-    public static class FilesNames
+    public static class Sprites
     {
         #region FOR EACH FILE
 
-        internal static string FILE_PATH_CAMEL_CASE => "FILE_PATH_RELATIVE";
+        public static string FILE_PATH_PASCAL_CASE => @"FILE_PATH_RELATIVE";
 
         #endregion
 
         #region FOR EACH FILE
 
-        public static string FILE_PATH_PASCAL_CASE => "FILE_PATH_RELATIVE";
+        public static Sprite FILE_PATH_CAMEL_CASE => GetSprite(@"FILE_PATH_RELATIVE");
 
         #endregion
 
+        private static Sprite GetSprite(string spriteName)
+        {
+            return NamespaceOfYourGameEngine.ContentLoad<Sprite>(spriteName);
+        }
+
+        #region FOR EACH FILE
+
+        public static void UnloadFILE_PATH_PASCAL_CASE(string spritePath)
+        {
+            NamespaceOfYourGameEngine.ContentUnloader<Sprite>(@"FILE_PATH_RELATIVE");
+        }
+
+        #endregion
     }
 }
